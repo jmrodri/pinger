a2ba086febebf5f36bc4468aeb3d50e6e51538a3
jesus m. rodriguez <jmrodri@gmail.com>
2018-09-18 16:37:56 -0400

    pinger container

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+USER_ID=$(id -u)
+if [ ${USER_UID} != ${USER_ID} ]; then
+  sed "s@${USER_NAME}:x:\${USER_ID}:@${USER_NAME}:x:${USER_ID}:@g" ${BASE_DIR}/etc/passwd.template > /etc/passwd
+fi
+
+exec /usr/bin/pinger.sh
---
+++ pinger.sh
@@ -0,0 +1,1 @@
+/usr/bin/ping -4 zeusville.homeip.net
