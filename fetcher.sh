c794b25b9dfd57d9f227ef2b8d4ffe09b163b284
jesus m. rodriguez <jesusr@redhat.com>
2018-09-20 16:24:09 -0400

    show error but not progress

--- fetcher.sh
+++ fetcher.sh
@@ -1,6 +1,6 @@
 while true; do
     echo "Fetching testfile"
-    curl --output `date --iso-8601=seconds` --connect-timeout 2 -s "server.nsa:9090/testfile"
+    curl --output `date --iso-8601=seconds` --connect-timeout 2 -s -S "server.nsa:9090/testfile"
     echo "Sleeping 2 seconds"
     sleep 2
 done
