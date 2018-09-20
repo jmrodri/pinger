5aff4cc8deb1660fea23fd92b6afeeb514935a36
jesus m. rodriguez <jesusr@redhat.com>
2018-09-20 14:46:57 -0400

    add timeout, sleep less

--- fetcher.sh
+++ fetcher.sh
@@ -1,6 +1,6 @@
 while true; do
     echo "Fetching testfile"
-    curl --output `date --iso-8601=seconds` -s "server.nsa:9090/testfile"
-    echo "Sleeping 10 seconds"
-    sleep 10
+    curl --output `date --iso-8601=seconds` --connect-timeout 2 -s "server.nsa:9090/testfile"
+    echo "Sleeping 2 seconds"
+    sleep 2
 done
